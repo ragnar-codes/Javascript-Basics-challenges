0a3a0f57b0fb8d5e3fb1d268b2708f311aac2677
ragnar0809 <yessine.agrebi.aiesec@gmail.com>
2021-06-01 23:01:05 +0200

    challenge 9

---
+++ challenge9.js
@@ -0,0 +1,9 @@
+// Write a function that takes a number as argument
+// Round the number to the 2nd digit after the comma
+// Return the rounded number
+
+function myFunction(x) {
+    console.log(x.toFixed(2));
+}
+
+myFunction(2.12397);
