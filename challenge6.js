efe00268b3a4707d9d38fe2598712057ff83aee4
ragnar0809 <yessine.agrebi.aiesec@gmail.com>
2021-06-01 19:27:09 +0200

    challenge 6

---
+++ challenge6.js
@@ -0,0 +1,17 @@
+// Write a function that takes a number as argument
+// The function should split the number into its individual digits and return them in an array
+// Tipp: you might want to change the type of the number for the splitting
+
+function myFunction(num) {
+    let str =  num.toString();
+    let arr = [];
+    for(let i = 0; i < str.length; i++){
+        arr.push(str[i]);
+    }
+    return arr;
+    
+}
+
+
+console.log(myFunction(1800));
+
