7ae2c906f9e33de9aa7ef5e065d9ca83d25212f3
ragnar0809 <yessine.agrebi.aiesec@gmail.com>
2021-06-02 18:14:54 +0200

    challenge 16

--- Javascript Arrays/challenge16.js
+++ Javascript Arrays/challenge16.js
@@ -2,9 +2,11 @@
 // It should return the last n array elements
 // If the array has less than n elements, return all
 
-function myFunction(arr) {
-    let last = arr.length - 1;
-    return arr[last];
+function myFunction(arr, n) {
+    if(n > arr.length){
+        return arr;
+    }
+    return arr.slice(-n)
 }
 
-console.log(myFunction([1, 2, 3]));
+console.log(myFunction([1, 2, 3, 4, 5], 2));
